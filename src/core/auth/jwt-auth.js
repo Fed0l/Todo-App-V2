2752adc1a4aa222114e104ac95484e2f8962a45b
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-08-23 18:17:43 +0330

    update comments

--- src/core/auth/jwt-auth.js
+++ src/core/auth/jwt-auth.js
@@ -1,24 +1,23 @@
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../secrets/secrets.js';
+import jwt from 'jsonwebtoken';  // Import the jsonwebtoken library for handling JWTs
+import { JWT_SECRET } from '../secrets/secrets.js';  // Import secret key configuration for JWT signing
 
-// sign data and create jwt token
-function jwtSign(data){
-    const signKeySecret = JWT_SECRET.signKey;
-    return jwt.sign(data, signKeySecret,{expiresIn: '7d'});
+// Function to sign data and create a JWT token
+function jwtSign(data) {
+    const signKeySecret = JWT_SECRET.signKey;  // Retrieve the signing key from the secret configuration
+    return jwt.sign(data, signKeySecret, { expiresIn: '7d' });  // Sign the data with the key and set the token to expire in 7 days
 }
 
-// validate jwt Token can throw error if not match
-function jwtValidate(jwtToken){
+// Function to validate a JWT token; throws an error if the token is invalid
+function jwtValidate(jwtToken) {
     try {
-        return jwt.verify(jwtToken , JWT_SECRET.signKey);
+        return jwt.verify(jwtToken, JWT_SECRET.signKey);  // Verify the JWT token using the signing key
     } catch (error) {
-        console.log(error);
-        throw new Error('Invalid JWT Token!')
+        console.log(error);  // Log the error if verification fails
+        throw new Error('Invalid JWT Token!');  // Throw an error indicating that the token is invalid
     }
 }
 
-export{
-    jwtSign,
-    jwtValidate
+export {
+    jwtSign,     // Export the function to sign JWT tokens
+    jwtValidate  // Export the function to validate JWT tokens
 }
-
