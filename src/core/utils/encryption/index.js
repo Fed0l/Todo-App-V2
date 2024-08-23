36e34ea5a8fc32062a487a6873f92309cbcc832c
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-08-23 18:24:06 +0330

    add coments

--- src/core/utils/encryption/index.js
+++ src/core/utils/encryption/index.js
@@ -1,28 +1,33 @@
 import bcrypt from 'bcrypt';
 import { BCRYPT_CONFIG } from '../../configs/index.js';
-// hash passwords
-async function hash(input){
+
+// Hashes the input password using bcrypt with configured salt rounds
+async function hash(input) {
     try {
+        // Generate a salt using the number of rounds specified in the config
         const salt = await bcrypt.genSalt(BCRYPT_CONFIG.rounds);
+        // Hash the input using the generated salt
         const hash = await bcrypt.hash(input, salt);
-        return hash;
+        return hash;  // Return the hashed password
     } catch (error) {
         console.log(error);
-        return null;
-    }
-}
-// validate passwords
-async function validatHash(input, hash){
-    try {
-        const compare = await bcrypt.compare(input, hash)
-        return compare;
-    } catch (error) {
-        console.log(error);
-        return null;
+        return null;  // Return null if an error occurs
     }
 }
 
-export{
-    hash,
-    validatHash
+// Validates the input password against a hashed password
+async function validatHash(input, hash) {
+    try {
+        // Compare the input password with the stored hash
+        const compare = await bcrypt.compare(input, hash);
+        return compare;  // Return true if the passwords match, otherwise false
+    } catch (error) {
+        console.log(error);
+        return null;  // Return null if an error occurs
+    }
+}
+
+export {
+    hash,          // Export the hash function for password hashing
+    validatHash    // Export the validatHash function for password validation
 }
