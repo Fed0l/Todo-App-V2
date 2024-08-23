2924716d495317d6452ec9fe1f28c985cb7c245d
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-08-23 18:18:50 +0330

    add coment

--- src/core/configs/index.js
+++ src/core/configs/index.js
@@ -1,7 +1,14 @@
+// Configuration for the Express application
 const EXPRESS_APP = {
-    port: process.env['PORT'],
-  }
-  
-  export {
-    EXPRESS_APP
-  };
+  port: process.env['PORT'],  // Set the port for the server from the environment variable 'PORT'
+}
+
+// Configuration for Bcrypt (password hashing)
+const BCRYPT_CONFIG = {
+  rounds: +process.env['BCRYPT_ROUNDS']  // Set the number of hashing rounds from the environment variable 'BCRYPT_ROUNDS', converting it to a number
+}
+
+export {
+  EXPRESS_APP,   // Export the Express app configuration
+  BCRYPT_CONFIG  // Export the Bcrypt configuration
+}
