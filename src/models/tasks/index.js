2771b1d8a21a7c81f1eade3e61a5e6c59af84375
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-08-23 17:22:42 +0330

    update some queries in model folders for tasks

--- src/models/tasks/index.js
+++ src/models/tasks/index.js
@@ -4,30 +4,36 @@ const schema = "public";
 const table = "tasks";
 
 async function getAllTaskByUserID(user_id) {
-  const sql = (`select * from ${schema}.${table} 
-    where user_id =$1;`);
-  const res = query(sql, [user_id]);
-  return res;
+  const sql = `select * from ${schema}.${table} 
+    where userid =$1;`;
+  const res = await query(sql, [user_id]);
+  return res.rows;
 }
 
-async function getTaskById (id){
-  const sql = (`select * from ${schema}.${table} 
-    where id =$1;`);
-  const res = query(sql,[id]);
-  return res;
+async function getTaskById(id) {
+  const sql = `select * from ${schema}.${table} 
+    where id =$1;`;
+  const res = await query(sql, [id]);
+  return res.rows;
 }
 
-async function createTask(user_id, title, description, duedate, is_completed){
-  const sql = (`INSERT INTO 
+async function createTask(user_id, title, description, duedate, is_completed) {
+  const sql = `INSERT INTO 
     ${schema}.${table} 
-    (user_id, title, description, duedate, is_completed) 
-    VALUES ($1, $2, $3, $4, $5);`);
-  const res = query(sql, [user_id, title, description, duedate, is_completed])
+    (userid, title, description, duedate, is_completed) 
+    VALUES ($1, $2, $3, $4, $5);`;
+  const res = await query(sql, [
+    user_id,
+    title,
+    description,
+    duedate,
+    is_completed,
+  ]);
 }
 
-async function updateTaskById(user_id, title, description,duedate, is_completed) {
-  const res = await getAllTaskByUserID(user_id);
-  console.log(res);
+async function updateTaskById(id, title, description, duedate, is_completed) {
+  const res = await getTaskById(id);
+  console.log("res", res);
   let sql;
   if (
     res[0].title != title ||
@@ -36,24 +42,23 @@ async function updateTaskById(user_id, title, description,duedate, is_completed)
     res[0].is_completed != is_completed
   ) {
     sql = `update ${schema}.${table}
-    set title = $2 , description =$3, duedate = $4, is_completed = $5 where user_id =$1 and title = ${res[0].title} ;`;
+    set title = $2 , description =$3, duedate = $4, is_completed = $5 where id =$1;`;
   }
-  const result = await query(sql, [user_id, title, description, duedate, is_completed]);
+  const result = await query(sql, [
+    id,
+    title,
+    description,
+    duedate,
+    is_completed,
+  ]);
   return result;
 }
 
-async function deleteTaskById(user_id,title) {
+async function deleteTaskById(id) {
   const sql = `delete from ${schema}.${table}
-     where user_id = $1 and title = $2;`;
-    const res = await query(sql, [user_id, title]);
-    return res
+     where id = $1;`;
+  const res = await query(sql, [id]);
+  return res;
 }
 
-export {
-  getAllTaskByUserID,
-  getTaskById,
-  createTask,
-  // updateTaskById,
-  deleteTaskById
-}
-
+export { getAllTaskByUserID, createTask, updateTaskById, deleteTaskById };
